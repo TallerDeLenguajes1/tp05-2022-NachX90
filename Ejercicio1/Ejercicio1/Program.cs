b3c1925de2089cadedb5d13e0418713b92f2900a
NachX90 <nachx90@protonmail.com>
2022-05-22 19:54:44 -0300

    Ejercicio 1

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("INVERSOR DE NUMEROS MAYORES A CERO");
+Console.WriteLine("Ingrese el numero a invertir:");
+int numeroAInvertir = int.Parse(Console.ReadLine());
+if (numeroAInvertir <= 0)
+{
+    Console.WriteLine("El numero es cero o negativo. No se invertira.");
+}
+else
+{
+    int numeroInvertido=0;
+    while (numeroAInvertir > 0)
+    {
+        numeroInvertido = numeroInvertido * 10 + (numeroAInvertir % 10);
+        numeroAInvertir /= 10;
+    }
+    Console.WriteLine("El numero invertido es: "+numeroInvertido);
+}
