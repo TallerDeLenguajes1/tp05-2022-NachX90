f9ec17dcb883ecc49ba1a3f0729dd6521d382c3e
NachX90 <nachx90@protonmail.com>
2022-05-22 22:35:44 -0300

    Ejercicio 2

--- Ejercicio1/Ejercicio1/Program.cs
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -1,17 +1,42 @@
-﻿Console.WriteLine("INVERSOR DE NUMEROS MAYORES A CERO");
-Console.WriteLine("Ingrese el numero a invertir:");
-int numeroAInvertir = int.Parse(Console.ReadLine());
-if (numeroAInvertir <= 0)
+﻿Console.WriteLine("CALCULADORA V1.0");
+string seguir;
+do
 {
-    Console.WriteLine("El numero es cero o negativo. No se invertira.");
-}
-else
-{
-    int numeroInvertido=0;
-    while (numeroAInvertir > 0)
+    string opcion;
+    do
     {
-        numeroInvertido = numeroInvertido * 10 + (numeroAInvertir % 10);
-        numeroAInvertir /= 10;
+        Console.WriteLine("\n\t1: Suma\n\t2: Resta\n\t3: Producto\n\t4: Cociente");
+        Console.Write("Ingrese la operacion a realizar: ");
+        opcion = Console.ReadLine();
+    } while (opcion != "1" && opcion != "2" && opcion != "3" && opcion != "4");
+    Console.Write("Escriba el primer numero: ");
+    int numero1 = int.Parse(Console.ReadLine());
+    Console.Write("Escriba el segundo numero: ");
+    int numero2 = int.Parse(Console.ReadLine());
+    switch (opcion){
+        case "1":
+            Console.WriteLine($"\nEl resultado de la suma {numero1} + {numero2} es: " + (numero1 + numero2));
+            break;
+        case "2":
+            Console.WriteLine($"\nEl resultado de la resta {numero1} - {numero2} es: " + (numero1 - numero2));
+            break;
+        case "3":
+            Console.WriteLine($"\nEl resultado del producto {numero1} * {numero2} es: " + (numero1 * numero2));
+            break;
+        case "4":
+            if (numero2 == 0)
+            {
+                Console.WriteLine("\nIndefinido. No se puede dividir en cero");
+            }
+            else
+            {
+                Console.WriteLine($"\nEl resultado del cociente {numero1} / {numero2} es: " + (numero1 / numero2));
+            }
+            break;
+        default:
+            break;
     }
-    Console.WriteLine("El numero invertido es: "+numeroInvertido);
-}
+    Console.Write("\nDesea hacer otra operacion? (presione 1 para si, cualquier otra tecla para no): ");
+    seguir = Console.ReadLine();
+} while (seguir == "1");
+Console.WriteLine("Gracias por utilizar. Vuelva pronto.");
