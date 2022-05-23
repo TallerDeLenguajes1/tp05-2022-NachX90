050db56dd1bcc18ea8443c448559b4e485472a52
NachX90 <nachx90@protonmail.com>
2022-05-23 01:14:49 -0300

    Ejercicio 1 terminado. Carpetas renombradas

---
+++ MiAplicacion/Ejercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("INVERSOR DE NÚMEROS MAYORES A CERO");
+Console.Write("Ingrese el número a invertir: ");
+int numeroAInvertir = int.Parse(Console.ReadLine());
+if (numeroAInvertir <= 0)
+{
+    Console.WriteLine("El número ingresado es cero o negativo. No se invertirá.");
+}
+else
+{
+    int numeroInvertido=0;
+    while (numeroAInvertir > 0)
+    {
+        numeroInvertido = numeroInvertido * 10 + (numeroAInvertir % 10);
+        numeroAInvertir /= 10;
+    }
+    Console.WriteLine("El número invertido es: " + numeroInvertido);
+}
